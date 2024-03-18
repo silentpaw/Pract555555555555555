df16274152f219e84fa2ce40904e10502f138e4f
silentpaw <145987682+silentpaw@users.noreply.github.com>
2024-03-18 16:44:44 +0300

    Add files via upload

---
+++ MyApplication5554/app/src/main/java/com/example/myapplication5554/AppleActivity.java
@@ -0,0 +1,25 @@
+package com.example.myapplication5554;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class AppleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_apple);
+    }
+    public void OnMain(View v){
+        Button Main = findViewById(R.id.Btn_Main);
+        Intent intent = new Intent(this, MainActivity.class);
+        Main.setOnClickListener(view->{
+            startActivity(intent);
+        });
+
+    }
+}
---
+++ MyApplication5554/app/src/main/java/com/example/myapplication5554/TomatoesActivity.java
@@ -0,0 +1,25 @@
+package com.example.myapplication5554;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class TomatoesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tomatoes);
+    }
+    public void OnMain(View v){
+        Button Main = findViewById(R.id.Btn_Main);
+        Intent intent = new Intent(this, MainActivity.class);
+        Main.setOnClickListener(view->{
+            startActivity(intent);
+        });
+
+    }
+}
